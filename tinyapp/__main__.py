753faafe6f05ef95d6f42a2826b9d539b9ad42a4
Andrew Plotkin <zarf@ZarfLent.local>
2024-11-09 10:09:20 -0500

    Minimal 404-ifier.

--- tinyapp/__main__.py
+++ tinyapp/__main__.py
@@ -8,9 +8,29 @@ import werkzeug.serving
 parser = argparse.ArgumentParser()
 
 parser.add_argument('filename')
+parser.add_argument('-u', '--uri', dest='uri')
+parser.add_argument('-d', '--dir', dest='dir')
 
 args = parser.parse_args()
 
+### https://werkzeug.palletsprojects.com/en/stable/middleware/shared_data/
+
+def application(environ, start_response):
+    print('###', environ.get('PATH_INFO', '???'))
+    
+    request_uri = environ.get('REQUEST_URI', '???')
+    status = '404 Not Found'
+    msg = '###Not found: %s' % (request_uri,)
+    output = status + '\n\n' + msg
+    boutput = output.encode()
+    content_type = 'text/plain; charset=utf-8'
+    
+    response_headers = [
+        ('Content-Type', content_type),
+        ('Content-Length', str(len(boutput)))
+    ]
+    start_response(status, response_headers)
+    yield boutput
 
 loader = importlib.machinery.SourceFileLoader('_wsgiapp', args.filename)
 spec = importlib.util.spec_from_loader('_wsgiapp', loader=loader)
@@ -18,5 +38,5 @@ appmod = importlib.util.module_from_spec(spec)
 sys.modules['_wsgiapp'] = appmod
 spec.loader.exec_module(appmod)
 
-werkzeug.serving.run_simple('localhost', 8001, appmod.application)
+werkzeug.serving.run_simple('localhost', 8001, application)
 
